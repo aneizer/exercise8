bf939f260c214b0f5398d71b46077314d2a3ce76
Get into Tech <abiamonooneizer@gmail.com>
2023-01-22 22:01:33 +0000

    Finished adding comments

--- say_my_name.py
+++ say_my_name.py
@@ -3,10 +3,11 @@
 first_name = 'Abigail'
 last_name = 'Amonoo-Neizer'
 
-# using the print function to output full name with a space
+# using the print function to output my full name with a space
 print(first_name + ' ' + last_name)
 
-# creating another variable with
+# creating another variable defined with a space at the end of my first name
 first_name2 = 'Abigail '
 
+# using print function again to output my full name
 print(first_name2 + last_name)
