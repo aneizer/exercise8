cab182dba10a8187f70635d1afa12d3b36cd68e2
Get into Tech <abiamonooneizer@gmail.com>
2023-01-22 21:28:08 +0000

    Storing say_my_name into the repo

---
+++ say_my_name.py
@@ -0,0 +1,12 @@
+
+# Creating variables that are defined as first and last name
+first_name = 'Abigail'
+last_name = 'Amonoo-Neizer'
+
+# using the print function to output full name with a space
+print(first_name + ' ' + last_name)
+
+# creating another variable with
+first_name2 = 'Abigail '
+
+print(first_name2 + last_name)
